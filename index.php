7463b36b87cd1bff2cfa31c18e798d33c0248847
mindfullsilence <voltecdesign@gmail.com>
2014-12-17 14:24:43 -0700

    sidebar and footer widget areas

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 $context['widgets']['sidebar'] = Timber::get_widgets('Sidebar'); // https://github.com/jarednova/timber/wiki/Sidebar#method-3-dynamic
-$context['widgets']['sidebar'] = Timber::get_widgets('Footer');
+$context['widgets']['footer'] = Timber::get_widgets('Footer');
 $templates = array('index.twig');
 Timber::render($templates, $context);
