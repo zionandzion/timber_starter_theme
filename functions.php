8a5f8a727b6648cc424f8a94812b8d27f5ca091c
mindfullsilence <voltecdesign@gmail.com>
2014-12-17 17:38:55 -0700

    ability to add new filters

--- functions.php
+++ functions.php
@@ -1,63 +1,67 @@
 <?php
 require_once dirname( __FILE__ ) . '/dependencies/tgm-plugin-activation.php';
 
-if (!class_exists('Timber')){
-	add_action( 'admin_notices', function(){
-		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . admin_url('plugins.php#timber') . '">' . admin_url('plugins.php') . '</a></p></div>';
-	});
-	return;
-}
-
 class StarterSite extends TimberSite {
 	public $assets = '';
+	public $customTwig = '';
 
 	function __construct(){
 		$this->assets = get_template_directory_uri() . '/assets/production';
-			add_theme_support('post-formats');
-		add_theme_support('post-thumbnails');
-		add_theme_support('menus');
-		add_filter('timber_context', array($this, 'add_to_context'));
-		add_filter('get_twig', array($this, 'add_to_twig'));
-		add_action('wp_enqueue_scripts', array($this, 'register_scripts'));
-		add_action('widgets_init', array($this, 'register_sidebars'));
-		add_action( 'tgmpa_register', array($this, 'register_required_plugins'));
+		$this->customTwig = new CustomTwig();
+
+		add_action('after_setup_theme', array($this, 'setup'));
 		parent::__construct();
 	}
 
+	function setup() {
+		add_theme_support('post-formats');
+		add_theme_support('post-thumbnails');
+		add_theme_support('menus');
+
+		add_filter('timber_context', array($this, 'add_to_context'));
+		add_filter('get_twig',       array($this->customTwig, 'construct'));
+
+		add_action('wp_enqueue_scripts', array($this, 'laod_scripts'));
+		add_action('widgets_init',       array($this, 'register_widget_areas'));
+		add_action('tgmpa_register',     array($this, 'register_required_plugins'));
+	}
+
 	// scripts and styles
-	function register_scripts() {
-		wp_deregister_script('jquery');
-		wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', false, null, true);
-		wp_enqueue_script('jquery');
-		wp_register_script('theme_scripts');
-		wp_enqueue_script('theme_scripts', $this->assets . '/js/scripts.js', false, null, true);
-		wp_register_style('theme_styles');
-		wp_enqueue_style('theme_styles', $this->assets . '/css/styles.css', false, 'all');
+	function load_scripts() {
+		if( !is_admin() ) {
+			wp_deregister_script('jquery');
+
+			wp_register_script('jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', false, null, true);
+			wp_enqueue_script('jquery');
+
+			wp_register_script('theme_scripts');
+			wp_enqueue_script('theme_scripts', $this->assets . '/js/scripts.js', false, null, true);
+
+			wp_register_style('theme_styles');
+			wp_enqueue_style('theme_styles', $this->assets . '/css/styles.css', false, 'all');
+		}
 	}
 
 	// sidebars / widget areas
-	function register_sidebars() {
+	function register_widget_areas() {
+		$defaults = array(
+			'before_title' => '',
+			'after_title' => '',
+			'before_widget' => '',
+			'after_widget' => ''
+		);
 		$sidebars = array(
 			array(
 				'name' => 'Sidebar',
-				'id' => 'sidebar',
-				'description' => __('Shows to the right of your content.', 'zz'),
-				'before_title' => '',
-				'after_title' => '',
-				'before_widget' => '',
-				'after_widget' => ''
+				'id' => 'sidebar'
 			),
 			array(
 				'name' => 'Footer',
-				'id' => 'footer',
-				'description' => __('Shows at the bottom of your site.', 'zz'),
-				'before_title' => '',
-				'after_title' => '',
-				'before_widget' => '',
-				'after_widget' => ''
+				'id' => 'footer'
 			)
 		);
 		foreach($sidebars as $args) {
+			$args = $args + $defaults;
 			register_sidebar($args);
 		}
 	}
@@ -112,24 +116,53 @@ class StarterSite extends TimberSite {
 
 	// global context
 	function add_to_context($context){
-		$context['menu'] = new TimberMenu();
 		$context['site'] = $this;
+		$context['widgets']['footer'] = Timber::get_widgets('Footer');
 		return $context;
-	}
-
-	// twig filters/functions
-	function add_to_twig($twig){
-		$twig->addExtension(new Twig_Extension_StringLoader());
-		$twig->addFilter('debug', new Twig_Filter_Function('debug'));
-		return $twig;
 	}
 }
 new StarterSite();
-function debug($msg)
-{
-	echo '<code class="container">';
-	echo '<pre class="well">';
-	print_r($msg);
-	echo '</pre>';
-	echo '</code>';
+
+
+class CustomTwig {
+	public $filters;
+	function construct($twig) {
+		
+		// associate twig name and function name here
+		$this->filters = array(
+			array(
+				'twig_string' => 'debug', // in twig: {{ somevar|debug }}
+				'function' => array($this, 'debug')
+			),
+			array(
+				'twig_string' => 'social', // e.g. {{ social|facebook }}
+				'function' => array($this, 'social_media_icons')
+			)
+		);
+
+		$twig->addExtension(new Twig_Extension_StringLoader());
+		foreach($this->filters as $item) {
+			$args = array($this, $item['function']);
+			$filter = new Twig_SimpleFilter($item['twig_string'], $args);
+			$twig->addFilter($filter);
+		}
+	}
+
+	// Add new functions, filters and tests below.
+	function debug($message) {
+		echo '<code class="container">';
+		echo '<pre class="well">';
+		print_r($message);
+		echo '</pre>';
+		echo '</code>';
+	}
+
+	function secial_media_icons($link, $title) {
+		$share = array(
+			'facebook' => "http://www.facebook.com/share.php?u=$link&title=$title",
+			'twitter' => "http://twitter.com/home?status=$title+$link",
+			'google_plus' => "https://plus.google.com/share?url=$link"
+		);
+		return array('share' => $share);
+	}
 }
